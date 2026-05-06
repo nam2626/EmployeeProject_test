c803958a91f0e4f3017d1418951c34c43178bfbc
NamSangKyu <nam2626@naver.com>
2026-05-06 14:16:11 +0900

    feat : 사원정보 추가 기능 작업 중

---
+++ src/main/java/controller/AppendController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.util.Scanner;
+
+public class AppendController implements Controller {
+    @Override
+    public void execute(Scanner sc) {
+        System.out.println("사원 정보를 추가합니다.....");
+        System.out.print("등록할 사원번호 : ");
+        String id = sc.nextLine();
+
+        //중복체크
+
+    }
+}

2624ed34f6efe67162ee0ec061a206f916ceeb8a
NamSangKyu <nam2626@naver.com>
2026-05-06 14:43:24 +0900

    feat : 사원정보 추가 기능 작업 완료

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package service;
 
+import exception.EmployeeException;
 import vo.EmployeeVO;
 
 import java.io.BufferedReader;
@@ -46,5 +47,18 @@ public class EmployeeService {
         if (instance == null)
             instance = new EmployeeService();
         return instance;
+    }
+
+    public void checkDuplicateEmployeeId(String id) throws EmployeeException {
+
+        int idx = list.indexOf(new EmployeeVO(id,null,null,0,null));
+
+        if(idx != -1)
+            throw new EmployeeException("사원번호가 중복되었습니다.");
+
+    }
+
+    public boolean appendEmployee(EmployeeVO employeeVO) {
+        return list.add(employeeVO);
     }
 }
